d6b68bec582a3682974f8af9a9b354b86fe8de01
DMStha <109300299+DMStha@users.noreply.github.com>
2025-03-05 23:12:12 +0545

    FUNCTIONS

---
+++ functions.js
@@ -0,0 +1,10 @@
+// Traditional Function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Arrow Function ES6
+const greetArrow = (name) => `Hello, ${name}!`;
+
+console.log(greet("John"));
+console.log(greetArrow("Doe"));
